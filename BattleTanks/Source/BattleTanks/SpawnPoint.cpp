1cc38cba5597e060b1c3a12adf496ecf61e82192
SaviorKai <55675002+SaviorKai@users.noreply.github.com>
2019-11-05 15:02:38 +1100

    286. SpawnActorDeferred and BeginPlay (SPAWNPOINT BUGFIX!)
    The previous way of spawning wasn't correct, since the SpawnActor code, also actioned the beginplay() code of the spawning actor, before we got to the next line of attaching the newly spawned actor, to a component.
    A way to post-pone the BeginPlay() code in the spawned actor is to use UWorld::SpawnActorDeferred(), followed by UGameplayStatics::FinishSpawningActor(), to initiate it's begin play.
    - https://docs.unrealengine.com/en-US/API/Runtime/Engine/Engine/UWorld/SpawnActorDeferred/index.html
    - https://docs.unrealengine.com/en-US/API/Runtime/Engine/Kismet/UGameplayStatics/FinishSpawningActor/index.html
    
    Changes:
    - Replaced the spawning code in Spawnpoint, top use SpawnActorDeferred() and FinishSpawningActor() instead.
    - Reset the location of the spawnpoint in the blueprint (it wasn't set to 0 properly, in relative space).
    - Used 'GetRelativeTransform()' instead of the lecture's suggestion to use 'GetComponentTransform(), and Change the AttachToComponent arguement to "FAttachmentTransformRules::KeepWorldTransform", instead of 'Relative'.

--- BattleTanks/Source/BattleTanks/SpawnPoint.cpp
+++ BattleTanks/Source/BattleTanks/SpawnPoint.cpp
@@ -4,6 +4,7 @@
 #include "SpawnPoint.h"
 #include "Engine/World.h"			// So we can use GetWorld()
 #include "GameFramework/Actor.h"	// So we can use GetAttachParentActor()
+#include "Kismet/GameplayStatics.h" // So we can use FinishSpawningActor()
 
 // Sets default values for this component's properties
 USpawnPoint::USpawnPoint()
@@ -20,10 +21,13 @@ void USpawnPoint::BeginPlay()
 	Super::BeginPlay();
 	
 	//Create a new Actor, and attach the root component, to this new actor.
-	auto NewActor = GetWorld()->SpawnActor<AActor>(SpawnClass);									//SpawnClass is a var we've created in the header file.
-	if (NewActor)																				//Pointer Protection
+	auto NewActor = GetWorld()->SpawnActorDeferred<AActor>(SpawnClass, GetRelativeTransform());		//SpawnActorDeferred, deferrs the BeginPlay() code, so that it only runs when we call it with UGameplayStatics::FinishSpawningActor()//NOTE: SpawnClass is a var we've created in the header file.
+	// auto NewActor = GetWorld()->SpawnActorDeferred<AActor>(SpawnClass, GetComponentTransform()); //I did not use this. If you use this, replace the instance below as well, and cahnge "KeepRelativeTransform" to "KeepWorldTransform"
+	
+	if (NewActor)																					//Pointer Protection
 	{
-		NewActor->AttachToComponent(this, FAttachmentTransformRules::KeepRelativeTransform);	//This attaches something to the "NewActor", not "NewActor" to something.
+		NewActor->AttachToComponent(this, FAttachmentTransformRules::KeepRelativeTransform);		//This attaches something to the "NewActor", not "NewActor" to something.
+		UGameplayStatics::FinishSpawningActor(NewActor, GetRelativeTransform());					//Finishes the spawn of the actor, by running it's BeginPlay() code now.
 	}
 	
 	
